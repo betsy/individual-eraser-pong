fa608e1ae47b640218bf77616cf0dd7fbac82f5d
miracle2 <tami.ybp@gmail.com>
2017-02-04 23:06:53 -0500

    cleaned up repeated code using imports. the code is now beautiful.

--- public/game.js
+++ public/game.js
@@ -2,8 +2,158 @@
 
     // your code goes here
 
-   exports.test = function(){
-        return 'hello world'
-    };
+exports.consts = {
+	width: 600,
+    height: 600,
+    friction: 0.8,
+    gravity: 1
+};
+
+exports.player = {
+  x : exports.consts.width/2,
+  y : exports.consts.height/2,
+  width : 50,
+  height : 50,
+  speed: 8,
+  velX: 0,
+  velY: 0,
+  jumping: false,
+  grounded: false
+};
+
+exports.make_boxes = function(){
+	 // dimensions
+	var boxes = [];
+	boxes.push({
+	    x: 300,
+	    y: 300,
+	    width: 10,
+	    height: 600
+	});
+	boxes.push({
+	    x: 0,
+	    y: 600 - 2,
+	    width: 600,
+	    height: 50
+	});
+	boxes.push({
+	    x: 600 - 10,
+	    y: 0,
+	    width: 50,
+	    height: 600
+	});
+
+	boxes.push({
+	    x: 420,
+	    y: 450,
+	    width: 80,
+	    height: 80
+	});
+	boxes.push({
+	    x: 170,
+	    y: 50,
+	    width: 80,
+	    height: 80
+	});
+	boxes.push({
+	    x: 220,
+	    y: 100,
+	    width: 80,
+	    height: 80
+	});
+	boxes.push({
+	    x: 270,
+	    y: 150,
+	    width: 40,
+	    height: 40
+	});
+	return boxes;
+};
+
+exports.update = function(player, boxes, keys){
+    // check keys
+    if (keys[38] || keys[32]) {
+        // up arrow or space
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            player.velY = -player.speed * 2;
+        }
+    }
+    if (keys[39]) {
+        // right arrow
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
+    }
+    if (keys[37]) {
+        // left arrow
+        if (player.velX > -player.speed) {
+            player.velX--;
+        }
+    }
+
+    player.velX *= exports.consts.friction;
+    player.velY += exports.consts.gravity;
+    
+    player.grounded = false;
+    for (var i = 0; i < boxes.length; i++) {
+        
+        var dir = colCheck(player, boxes[i]);
+
+        if (dir === "l" || dir === "r") {
+            player.velX = 0;
+            player.jumping = false;
+        } else if (dir === "b") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (dir === "t") {
+            player.velY *= -1;
+        }
+
+    }
+    
+    if(player.grounded){
+         player.velY = 0;
+    }
+    
+    player.x += player.velX;
+    player.y += player.velY;
+};
+
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null;
+
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        // figures out on which side we are colliding (top, bottom, left, or right)
+        var oX = hWidths - Math.abs(vX),
+            oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
 
 })(typeof exports === 'undefined'? this['game']={}: exports);
--- server.js
+++ server.js
@@ -10,8 +10,7 @@ var io = require('socket.io').listen(app.listen(PORT, function(){console.log("PO
 app.use(express.static('public'))
 
 
-var mymodule = require('./public/game.js');
-console.log(mymodule.test());
+var game = require('./public/game.js');
 
 // Loading the file index.html displayed to the client
 app.get('/', function (req, res) {
@@ -21,29 +20,16 @@ app.get('/', function (req, res) {
     });
 })
 
-var width = 600,
-    height = 600,
-    player = {
-      x : width/2,
-      y : height/2,
-      width : 50,
-      height : 50,
-      speed: 8,
-      velX: 0,
-      velY: 0,
-      jumping: false,
-      grounded: false
-    },
-    keys = [],
-    friction = 0.8,
-    gravity = 1.3;
+var width = game.consts.width,
+    height = game.consts.height,
+    player = game.player,
+    keys = [];
 
 
-// Loading socket.io
-
-
+var boxes = game.make_boxes();
 // var io = require('socket.io').listen(server);
 
+// Loading socket.io
 io.sockets.on('connection', function (socket, username) {
     // When the client connects, they are sent a message
     socket.emit('message', 'You are connected!');
@@ -61,162 +47,13 @@ io.sockets.on('connection', function (socket, username) {
         keys[message.keyCode] = message.val;
         // console.log(message);
         console.log(player.x+" "+player.y);
-        // update();
     }); 
     console.log("asdf");
     setInterval(function(){
-		update();
-	},20);
-	setInterval(function(){
-		socket.emit('player',player);
-	},40);
+        game.update(player, boxes, keys);
+    },20);
+    setInterval(function(){
+        socket.emit('player',player);
+    },40);
 });
 
-
-function make_boxes() {
-// dimensions
-var boxes = [];
-boxes.push({
-    x: 300,
-    y: 300,
-    width: 10,
-    height: 600
-});
-boxes.push({
-    x: 0,
-    y: 600 - 2,
-    width: 600,
-    height: 50
-});
-boxes.push({
-    x: 600 - 10,
-    y: 0,
-    width: 50,
-    height: 600
-});
-
-boxes.push({
-    x: 420,
-    y: 450,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 170,
-    y: 50,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 220,
-    y: 100,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 270,
-    y: 150,
-    width: 40,
-    height: 40
-});
-return boxes;
-}
-
-var boxes = make_boxes();
-
-function update() {
-    // check keys
-    if (keys[38] || keys[32]) {
-        // up arrow or space
-        if (!player.jumping && player.grounded) {
-            player.jumping = true;
-            player.grounded = false;
-            player.velY = -player.speed * 2;
-        }
-    }
-    if (keys[39]) {
-        // right arrow
-        if (player.velX < player.speed) {
-            player.velX++;
-        }
-    }
-    if (keys[37]) {
-        // left arrow
-        if (player.velX > -player.speed) {
-            player.velX--;
-        }
-    }
-
-    player.velX *= friction;
-    player.velY += gravity;
-
-    // ctx.clearRect(0, 0, width, height);
-    // ctx.fillStyle = "black";
-    // ctx.beginPath();
-    
-    player.grounded = false;
-    for (var i = 0; i < boxes.length; i++) {
-        // ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
-        
-        var dir = colCheck(player, boxes[i]);
-
-        if (dir === "l" || dir === "r") {
-            player.velX = 0;
-            player.jumping = false;
-        } else if (dir === "b") {
-            player.grounded = true;
-            player.jumping = false;
-        } else if (dir === "t") {
-            player.velY *= -1;
-        }
-
-    }
-    
-    if(player.grounded){
-         player.velY = 0;
-    }
-    
-    player.x += player.velX;
-    player.y += player.velY;
-
-    // ctx.fill();
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(player.x, player.y, player.width, player.height);
-
-    // requestAnimationFrame(update);
-}
-
-function colCheck(shapeA, shapeB) {
-    // get the vectors to check against
-    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
-        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
-        // add the half widths and half heights of the objects
-        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
-        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
-        colDir = null;
-
-    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
-    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
-        // figures out on which side we are colliding (top, bottom, left, or right)
-        var oX = hWidths - Math.abs(vX),
-            oY = hHeights - Math.abs(vY);
-        if (oX >= oY) {
-            if (vY > 0) {
-                colDir = "t";
-                shapeA.y += oY;
-            } else {
-                colDir = "b";
-                shapeA.y -= oY;
-            }
-        } else {
-            if (vX > 0) {
-                colDir = "l";
-                shapeA.x += oX;
-            } else {
-                colDir = "r";
-                shapeA.x -= oX;
-            }
-        }
-    }
-    return colDir;
-}
